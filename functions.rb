54e0987649fc098656a6a407cbceaf0c4c5b840e
Robert Brice <rob.a.brice@gmail.comC>
2017-01-16 13:34:03 +0000

    added class example

---
+++ functions.rb
@@ -0,0 +1,14 @@
+class  MyClass
+
+def initialize(value1, value2)
+  @value1 = value1
+  @value2 = value2
+end
+
+def product ()
+  n = @value1 * @value2
+  return n
+end
+
+
+end
--- ruby.rb
+++ ruby.rb
@@ -2,22 +2,20 @@
 Growing a Ruby Project and using GitHub from the command line
 =end
 
+require './functions.rb'
+
 puts "*************************************************"
 puts "Welcome to Robert's growing Ruby learning project"
 puts "Type an option number to run one of the projects"
-puts "*************************************************"
 puts ""
 puts "Enter 1 to say \"Hello World\" "
+puts "Enter 2 to get the product of two numbers"
 puts ""
 puts "More options coming ..."
 puts ""
+puts "*************************************************"
 
 userInput = gets.chomp.to_i 
-
-
-def newFunction()
-  puts "this is a stub for another function"
-end
 
 
 def helloWorld()
@@ -28,17 +26,30 @@ end
 
 
 
+
+
+
 # Got to watch that the main function that calls the others is at the end otherwise it wont work
 case userInput
 
 when 1
   helloWorld()
 
+when 2
+  puts "enter numeric values - which can be a float"
+  puts "Please enter value 1"
+  value1 = gets.chomp.to_f
+  puts "Please enter value 2"
+  value2 = gets.chomp.to_f
+  value = MyClass.new(value1, value2)
+  puts value.product()
+
 else
   puts "\nYour input must be an integer. Please make sure you have typed in your choice correctly."
   puts ""
 
 end
+
 
 
 
